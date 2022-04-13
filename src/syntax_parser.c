271c2f4e4d1b50b4cfc3c88603cb36f876ec8ed9
Joshua Comer <joshuagcomer@gmail.com>
2022-04-13 03:06:01 -0600

    Worked on parser. Needs reworking from scratch, but I'm glad I did this messy implementation first

--- src/syntax_parser.c
+++ src/syntax_parser.c
@@ -44,7 +44,7 @@ bool syntax_brackets_and_get_args(string_list_t * args, lexer_token_list_t * lis
         tok = list->tokens[list->index];
     }
 
-    if (tok == NULL || tok->token_str[0] != '[')
+    if (tok == NULL || tok->token_str[0] != ']')
     {
         setf_global_err("Input Error: Closing brace not encountered after opening brace. Token before is %s", list->tokens[list->index - 1]->token_str);
         return false;
@@ -67,13 +67,14 @@ bool _syntax_function_check_def_and_label(lexer_token_list_t * list)
         return false;
     }
 
-    if (list->index + 4 >= list->size)
+    if (list->index + 3 >= list->size)
     {
-        set_global_err("Input Error: Not enough tokens after def to create a function");
+        set_global_err("Input Error: Not enough tokens after def to create a variable or function");
         return false;
     }
 
     list->index++;
+    tok = list->tokens[list->index];
     
     if (tok->type != LEXER_TOKEN_T_LABEL)
     {
@@ -83,11 +84,26 @@ bool _syntax_function_check_def_and_label(lexer_token_list_t * list)
     
     list->index++;
 
-    // TODO: Remove the below line when function is finished
+    return true;
+}
+
+bool syntax_end_paren_or_statement(lexer_token_list_t * list)
+{
+    lexer_token_t * tok = list->tokens[list->index];
+
+    if (tok == NULL) // end of the statement
+        return true;
+    else if (tok->type == LEXER_TOKEN_T_PAREN && tok->token_str[0])
+    {
+        list->index++;
+        return true;
+    }
+    //else if (tok->type == LEXER_TOKEN_T_STATEMENT_END)
+
     return false;
 }
 
-bool syntax_function(lexer_token_list_t * list)
+bool syntax_label_def(string_list_t * variables, lexer_token_list_t * list)
 {
     if (list == NULL)
     {
@@ -95,14 +111,33 @@ bool syntax_function(lexer_token_list_t * list)
         return false;
     }
 
-    string_list_t * function_args = string_list_t_new();
-
     if (_syntax_function_check_def_and_label(list) == false)
         return false;
-    else if (syntax_brackets_and_get_args(function_args, list) == false)
+
+    if (list->index >= list->size)
+    {
+        set_global_err("Input Error: Defined label, but did not assign any value to it");
         return false;
-    else if (syntax_parenned_expression(function_args, list) == false)
-        return false;
+    }
+
+
+    lexer_token_t * tok = list->tokens[list->index];
+    // label is a function
+    if (tok->type == LEXER_TOKEN_T_BRACKET)
+    {
+        string_list_t * new_function_args = string_list_t_new();
+
+        if ( syntax_brackets_and_get_args(new_function_args, list) == false )
+            return false;
+        if ( syntax_parenned_expression(new_function_args, list) == false )
+            return false;
+    }
+    // label is a variable
+    else if (tok->type == LEXER_TOKEN_T_NUMBER || tok->type == LEXER_TOKEN_T_PAREN) // TODO: create syntax_expression() for either number or parenthases
+    {
+        if ( syntax_number(variables, list) == false )
+            return false;
+    }
 
     return true;
 }
@@ -125,17 +160,22 @@ bool syntax_matop(string_list_t * variables, lexer_token_list_t * list)
     }
 
     check_tok = list->tokens[list->index + 1];
+    list->index++;
     if (check_tok == NULL)
     {
         setf_global_err("Input Error: Expected a token after operator %s, but nothing was encountered", list->tokens[list->index]->token_str);
-    }
-    else if (check_tok->type != LEXER_TOKEN_T_NUMBER)
-    {
-        setf_global_err("Input Error: Expected a number after operator. Encountered %s", check_tok->token_str);
         return false;
     }
-
-    return true;
+    else if (check_tok->type == LEXER_TOKEN_T_NUMBER || (check_tok->type == LEXER_TOKEN_T_PAREN && check_tok->token_str[0] == '('))
+    {
+        list->index++;
+        return true;
+    }
+    else
+    {
+        setf_global_err("Input Error: Expected a number or expression after operator. Encountered %s", check_tok->token_str);
+        return false;
+    }
 }
 
 bool syntax_number(string_list_t * variables, lexer_token_list_t * list)
@@ -150,12 +190,13 @@ bool syntax_number(string_list_t * variables, lexer_token_list_t * list)
     if (tok->type == LEXER_TOKEN_T_LABEL)
     {
         // TODO: Check for distinction between functions and variables. Just a function name shouldn't work
-        if (string_list_t_contains(variables, tok->token_str))
-            return true;
-        else
-            return false;
+        return string_list_t_contains(variables, tok->token_str);
     }
-    if (tok->type != LEXER_TOKEN_T_NUMBER)
+    else if (tok->type == LEXER_TOKEN_T_PAREN && tok->token_str[0] == '(')
+    {
+        return syntax_parenned_expression(variables, list);
+    }
+    else if (tok->type != LEXER_TOKEN_T_NUMBER)
     {
         setf_global_err("Parser Error: A non number token was encountered when a number was expected. Encountered %s", tok->token_str);
         return false;
@@ -182,6 +223,21 @@ bool syntax_number(string_list_t * variables, lexer_token_list_t * list)
     }
 }
 
+bool syntax_keyword(string_list_t * functions, lexer_token_list_t * list)
+{
+    lexer_token_t * tok = list->tokens[list->index];
+
+    if ( strcmp(tok->token_str, "def") == 0 )
+    {
+        if ( syntax_label_def(functions, list) == false ) // label can correspond to either function or variable (which is a 0 argument function)
+            return false;
+        else
+            return true;
+    }
+
+    return false;
+}
+
 bool syntax_parenned_expression(string_list_t * functions, lexer_token_list_t * list)
 {
     if (list == NULL)
@@ -204,42 +260,42 @@ bool syntax_parenned_expression(string_list_t * functions, lexer_token_list_t * 
     while (tok != NULL && tok->token_str[0] != ')')
     {
         // nested parenthase'd expression
-        if (tok->type == LEXER_TOKEN_T_PAREN)
+        switch (tok->type)
         {
+        case LEXER_TOKEN_T_PAREN:
             if (syntax_parenned_expression(functions, list) == false)
                 return false;
-        }
-        else if (tok->type == LEXER_TOKEN_T_LABEL)
-        {
-            //if (syntax_label(list) == false)
-                return false;
-        }
-        else if (tok->type == LEXER_TOKEN_T_BRACKET)
-        {
-            set_global_err("Input Error: Encountered bracket inside a parenthase'd expression. Did not follow the correct rules of function definition");
-        }
-        else if (tok->type == LEXER_TOKEN_T_MATOP)
-        {
-            if (syntax_matop(functions, list) == false)
-                return false;
-        }
-        else if (tok->type == LEXER_TOKEN_T_KEYWORD)
-        {
-            //if (syntax_keyword(list) == false)
-                return false;
-        }
-        else if (tok->type == LEXER_TOKEN_T_NUMBER)
-        {
+            break;
+        case LEXER_TOKEN_T_LABEL:
             if (syntax_number(functions, list) == false)
                 return false;
+            break;
+        case LEXER_TOKEN_T_BRACKET:
+            set_global_err("Input Error: Encountered bracket inside a parenthase'd expression. Did not follow the correct rules of function definition");
+            return false;
+        case LEXER_TOKEN_T_MATOP:
+            if (syntax_matop(functions, list) == false)
+                return false;
+            break;
+        case LEXER_TOKEN_T_KEYWORD:
+            if (syntax_keyword(functions, list) == false)
+                return false;
+            break;
+        case LEXER_TOKEN_T_NUMBER:
+            if (syntax_number(functions, list) == false)
+                return false;
+            break;
         }
 
-
+        tok = list->tokens[list->index];
     }
 
-    // TODO: Remove below line when function is finished
-    return false;
-
+    if (tok == NULL)
+        return false;
+    else if (tok->token_str[0] == ')')
+        return true;
+    else
+        return false;
 }
 
 
@@ -284,9 +340,29 @@ int check_syntactically_correct(lexer_token_list_t * list)
             }
             case LEXER_TOKEN_T_BRACKET:
             {
-                // go back to the beginning of the function definition (the named label before opening bracket) for the syntax function to work properly
-                list->index--;
-                if ( syntax_function(list) == false )
+                
+                // this is where you would encounter brackets being used to pass arguments into a function
+                // it's at this point that I've decided this syntax_parser needs to be reworked. I'll start with a grammar for it
+
+                //// go back to the beginning of the function definition (the named label before opening bracket) for the syntax function to work properly
+                //list->index--;
+                //if ( syntax_function(list) == false )
+                //    return -424242;
+                //break;
+            }
+            case LEXER_TOKEN_T_PAREN:
+            {
+                if ( syntax_parenned_expression(seen_variables, list) == false )
+                    return -424242;
+                if ( list->tokens[list->index] == NULL || list->tokens[list->index]->type != LEXER_TOKEN_T_PAREN || list->tokens[list->index]->token_str[0] != ')' )
+                    return -424242;
+
+                list->index++;
+                break;
+            }
+            case LEXER_TOKEN_T_KEYWORD:
+            {
+                if ( syntax_keyword(seen_variables, list) == false )
                     return -424242;
                 break;
             }
